b2b39dc62155cca2b508389a89fe38e7ec53e536
XuanWu <37946721+demoswx@users.noreply.github.com>
2022-03-15 00:06:41 +0800

    initial

---
+++ index.js
@@ -0,0 +1,56 @@
+var userId ='' ;
+var token ='';
+var newPassword='';
+
+const check = () =>{
+    let inputValue1 = document.getElementById("pwd1").value; 
+    let inputValue2 = document.getElementById("pwd2").value; 
+ 
+    if(inputValue1==""||inputValue2=="")
+    {
+        alert("请将信息输入完整")
+    }
+    else if(inputValue1!=inputValue2){
+        alert("两次密码输入不一致请重输")
+    }
+    else{
+      
+
+      newPassword = document.getElementById('pwd1')
+    //   location.href="success.html"
+        
+        userId = getQueryVariable("userid");
+        token = getQueryVariable("token");
+        // postNetworkRequest();
+        alert("userid:"+userId)
+    }
+  }
+
+
+
+  
+
+function postNetworkRequest(){
+        $.ajax({
+            url: "http://localhost:8000/users/password-reset/"+userId+"/"+token,
+            data: {
+                newpassword: newPassword,
+
+            },
+            success: function( result ) {
+              $( "#TipArea" ).html( "<strong>" + result + "</strong> degrees" );
+            }
+          });
+    
+}
+
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
