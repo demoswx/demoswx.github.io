9ad26ad9a9619a4027cd4352d16b1a8ba1cc63f6
demoswx <gamemasterxuan.com>
2022-03-16 00:20:43 +0800

    update2

--- index.js
+++ index.js
@@ -9,13 +9,9 @@ let passwordinput1=document.getElementById("pwd1").value
 let passwordinput2=document.getElementById("pwd2").value
 userId = getQueryVariable("userid");
 token = getQueryVariable("token");
-
-
 function check(){
-    
     let inputValue1 = document.getElementById("pwd1").value; 
     let inputValue2 = document.getElementById("pwd2").value; 
- 
     if(inputValue1==""||inputValue2=="")
     {
         alert("请将信息输入完整")
@@ -25,15 +21,12 @@ function check(){
     }
     else{
         newPassword = document.getElementById('pwd1').value
+        console.log(2)
         console.log(userId+token);
         postNetworkRequest();
         console.log("success");     
     }
   }
-
-  
-   
-
 function postNetworkRequest(){
         $.ajax({
             type:'post',
